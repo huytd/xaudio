e7f6eb0beec1275e0af165f6e3c69fd1d7cf0544
Huy Tran <kingbazoka@gmail.com>
2020-12-22 21:18:31 -0800

    Update audio player UI

--- www_src/components/audio-player/index.tsx
+++ www_src/components/audio-player/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Fragment } from 'react';
 
 import {MediaPlayerContext} from '~/MediaPlayerState';
 import {SVG} from '~/components/svg';
@@ -8,10 +9,10 @@ import {durationDisplay} from '~/lib/utils';
 import {API} from '~/lib/api';
 
 import spinnerIcon from '~/img/spinner.svg';
-import playIcon from '~/img/play.svg';
-import pauseIcon from '~/img/pause.svg';
-import prevIcon from '~/img/prev.svg';
-import nextIcon from '~/img/next.svg';
+import playIcon from '~/img/play-icon.svg';
+import pauseIcon from '~/img/pause-icon.svg';
+import prevIcon from '~/img/backward-icon.svg';
+import nextIcon from '~/img/forward-icon.svg';
 
 export const AudioPlayer = () => {
   const {state, dispatch} = React.useContext(MediaPlayerContext);
@@ -176,42 +177,72 @@ export const AudioPlayer = () => {
 
   return (
     <div className="flex flex-row items-center flex-1 p-3 bg-gray-800 border-t border-gray-700">
-      <button
-        className="flex items-center justify-center w-8 h-8 mr-2 text-white bg-gray-600 rounded-full hover:bg-gray-500"
-        onClick={prevSongHandler}
-      >
-        <SVG content={prevIcon} />
-      </button>
-      <button
-        className="flex items-center justify-center w-12 h-12 mr-2 text-white bg-gray-600 rounded-full hover:bg-gray-500"
-        onClick={playPauseToggle}
-      >
-        <SVG content={playing ? pauseIcon : playIcon} />
-      </button>
-      <button
-        className="flex items-center justify-center w-8 h-8 mr-2 text-white bg-gray-600 rounded-full hover:bg-gray-500"
-        onClick={nextSongHandler}
-      >
-        <SVG content={nextIcon} />
-      </button>
-      <div className="px-3 font-mono text-sm text-center text-gray-500">
-        {durationDisplay(duration.current)}
+      {/* Left section */}
+      <div className="flex flex-row w-1/3">
+        {currentSongRef.current && (
+          <Fragment>
+            <div
+              className="flex-shrink-0 w-12 h-12 mr-3 bg-red-500"
+              style={{
+                backgroundImage: `url(https://img.youtube.com/vi/${currentSongRef.current?.id}/mqdefault.jpg)`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center center'
+              }}
+            />
+            <div className="self-center mr-2 text-sm text-white overflow-ellipsis">{currentSongRef.current?.title}</div>
+          </Fragment>
+        )}
       </div>
-      {loading ? (
-        <div className="flex-1 mx-5 text-sm text-center">
-          <div className="w-5 h-5 mx-auto text-white animate-spin">
-            <SVG content={spinnerIcon} />
+
+      {/* Center section */}
+      <div className={"w-1/3 flex flex-col items-center"}>
+        {/* Control buttons */}
+        <div className={"width-full flex flex-row items-center"}>
+          <button
+            className="flex items-center justify-center w-6 h-6 text-white"
+            onClick={prevSongHandler}
+          >
+            <SVG content={prevIcon} />
+          </button>
+          <button
+            className="flex items-center justify-center w-8 h-8 mx-2 text-white border border-white rounded-full"
+            onClick={playPauseToggle}
+          >
+            <SVG content={playing ? pauseIcon : playIcon} />
+          </button>
+          <button
+            className="flex items-center justify-center w-6 h-6 text-white"
+            onClick={nextSongHandler}
+          >
+            <SVG content={nextIcon} />
+          </button>
+        </div>
+        {/* Timeline */}
+        <div className={"w-full flex-1 mt-2 flex-shrink-0 flex flex-row items-center"}>
+          <div className="px-3 font-mono text-sm text-center text-gray-500">
+            {durationDisplay(duration.current)}
+          </div>
+          {loading ? (
+            <div className="flex-1 mx-5 text-sm text-center">
+              <div className="w-5 h-5 mx-auto text-white animate-spin">
+                <SVG content={spinnerIcon} />
+              </div>
+            </div>
+          ) : (
+            <ProgressBar progress={songProgress} onClick={songProgressClickHandler} />
+          )}
+          <div className="px-3 font-mono text-sm text-center text-gray-500">
+            {durationDisplay(duration.full)}
           </div>
         </div>
-      ) : (
-          <ProgressBar progress={songProgress} onClick={songProgressClickHandler} />
-        )}
-      <div className="px-3 font-mono text-sm text-center text-gray-500">
-        {durationDisplay(duration.full)}
       </div>
-      <div className="px-3 text-gray-500">
-        <input type="checkbox" name="direct-stream" className="mr-2" ref={directStreamRef} />
-        <label htmlFor="direct-stream" title="Select this option if you experienced slow connection issue">Direct Stream</label>
+
+      {/* Right section */}
+      <div className={"w-1/3 flex flex-row justify-end"}>
+        <div className="px-3 text-gray-500">
+          <input type="checkbox" name="direct-stream" className="mr-2" ref={directStreamRef} />
+          <label htmlFor="direct-stream" title="Select this option if you experienced slow connection issue">Direct Stream</label>
+        </div>
       </div>
     </div>
   );
