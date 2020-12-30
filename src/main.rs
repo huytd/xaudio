81c76459cbd1211a2a548b19864d868d7ac7b536
Huy Tran <kingbazoka@gmail.com>
2020-12-30 14:36:49 -0800

    First batch of Redis integration

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,45 @@ use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
 use actix_files::Files;
 use futures::StreamExt;
 
+use actix::prelude::*;
+use redis::{Client as RedisClient, aio::MultiplexedConnection};
+
+struct RedisActor {
+    conn: MultiplexedConnection,
+}
+
+impl RedisActor {
+    pub async fn new(redis_url: String) -> Self {
+        let client = RedisClient::open(redis_url).unwrap();
+        let (conn, call) = client.create_multiplexed_tokio_connection().await.unwrap();
+        actix_rt::spawn(call);
+        RedisActor { conn }
+    }
+}
+
+#[derive(Message, Debug)]
+#[rtype(result = "Result<Option<String>, redis::RedisError>")]
+struct InfoCommand;
+
+impl Handler<InfoCommand> for RedisActor {
+    type Result = ResponseFuture<Result<Option<String>, redis::RedisError>>;
+
+    fn handle(&mut self, _msg: InfoCommand, _: &mut Self::Context) -> Self::Result {
+        let mut con = self.conn.clone();
+        let cmd = redis::cmd("INFO");
+        let fut = async move {
+            cmd
+                .query_async(&mut con)
+                .await
+        };
+        Box::pin(fut)
+    }
+}
+
+impl Actor for RedisActor {
+    type Context = Context<Self>;
+}
+
 #[derive(Deserialize)]
 struct SearchQuery {
     query: String
@@ -87,18 +126,30 @@ async fn get_billboard() -> impl Responder {
     }
 }
 
-#[actix_web::main]
+#[get("/api/test")]
+async fn test(redis: web::Data<Addr<RedisActor>>) -> impl Responder {
+    let res = redis.send(InfoCommand).await.unwrap().unwrap().unwrap();
+    HttpResponse::Ok().body(res)
+}
+
+#[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let port = std::env::var("PORT").unwrap_or("3123".to_owned()).parse::<u16>().unwrap_or(3123);
+    let redis_port = std::env::var("REDIS_PORT").unwrap_or("6379".to_owned()).parse::<u16>().unwrap_or(6379);
+    let redis_path = format!("redis://0.0.0.0:{}", redis_port);
+    let redis_actor = RedisActor::new(redis_path).await;
+    let addr = redis_actor.start();
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .data(addr.clone())
             .service(search)
             .service(suggestion)
             .service(stream)
             .service(play)
             .service(get_billboard)
             .service(import_from_url)
+            .service(test)
             .service(Files::new("/", "./www").index_file("index.html"))
     })
     .bind(("0.0.0.0", port))?
