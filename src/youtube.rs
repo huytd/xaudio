5835a78d04f1ac5f9cddc2a2604904d4985b1164
Huy Tran <kingbazoka@gmail.com>
2020-12-29 23:04:03 -0800

    Use explicit file format

--- src/youtube.rs
+++ src/youtube.rs
@@ -94,7 +94,7 @@ pub fn get_song_url(id: &str) -> Result<String, String> {
     println!("DBG::FETCHING {}", url);
     let output = Command::new("youtube-dl")
     .arg("--get-url")
-    .arg("-f bestaudio")
+    .arg("-f bestaudio[ext=m4a]")
     .arg(url)
     .output().map_err(stringify_error)?;
     let stdout = String::from_utf8(output.stdout).map_err(stringify_error)?;
