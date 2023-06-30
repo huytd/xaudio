853fcb72c2c3f8d4f99b8fb64a7941342684b028
Huy <kingbazoka@gmail.com>
2023-06-30 15:41:18 +0000

    10 days playlist

--- src/cache.rs
+++ src/cache.rs
@@ -19,7 +19,7 @@ pub async fn read_from_redis(redis: web::Data<Addr<RedisActor>>, redis_pass: web
 pub async fn write_to_redis(redis: web::Data<Addr<RedisActor>>, redis_pass: web::Data<String>, key: String, data: String) {
     println!("DBG::WRITE REDIS {:?} {} {}", redis_pass, key, data);
     if let Ok(_auth_ret) = redis.send(RedisCommand(resp_array!["AUTH", redis_pass.as_str()])).await {
-        let result = redis.send(RedisCommand(resp_array!["SET", key, data, "EX", "86400"])).await;
+        let result = redis.send(RedisCommand(resp_array!["SET", key, data, "EX", "864000"])).await;
         match result {
             Ok(result) => println!("DBG::DATA SENT {:?}", result),
             Err(err) => println!("DBG::REDIS SEND ERROR {:?}", err)
